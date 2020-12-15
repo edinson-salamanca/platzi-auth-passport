831ced459526c9ce9c1d23677197bbbb9cc9a750
Edinson_Salamanca <sennin7k@gmail.com>
2020-12-15 14:19:36 -0500

    se agrego el listen

--- handle-session/index.js
+++ handle-session/index.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 
 const app = express();
 const fs = require('fs');
-fs.readFileSync
+fs.readFileSync;
 app.use(
   session({
     resave: false,
@@ -15,4 +15,8 @@ app.use(
 app.get('/', (req, res) => {
   req.session.count = req.session.count ? req.session.count + 1 : 1;
   res.status(200).json({ hello: 'mundo', counter: req.session.count });
+});
+
+app.listen(3000, () => {
+  console.log('Listening localhost:3000');
 });
