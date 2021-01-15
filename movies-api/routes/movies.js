05a7f7a3191573a9160a053f143d0c684eea27c2
Edinson_Salamanca <sennin7k@gmail.com>
2021-01-14 19:43:09 -0500

    Middleware para el manejode scopes

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -10,6 +10,7 @@ const {
 
 const validationHandler = require('../utils/middleware/validationHandler');
 const buildMessage = require('../utils/buildMessage');
+const scopesValidationHandler = require('../utils/middleware/scopesValidationHandler');
 
 const cacheResponse = require('../utils/cacheResponse');
 const {
@@ -29,6 +30,7 @@ const moviesApi = (app) => {
   router.get(
     '/',
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['read:movies']),
     async function (req, res, next) {
       cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
 
@@ -50,6 +52,7 @@ const moviesApi = (app) => {
   router.get(
     '/:movieId',
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['read:movies']),
     validationHandler({ movieId: movieIdSchema }, 'params'),
     async function (req, res, next) {
       cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
@@ -72,6 +75,7 @@ const moviesApi = (app) => {
   router.post(
     '/',
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['create:movie']),
     validationHandler(createMovieSchema),
     async function (req, res, next) {
       const { body: movie } = req;
@@ -92,6 +96,7 @@ const moviesApi = (app) => {
   router.put(
     '/:movieId',
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['update:movies']),
     validationHandler({ movieId: movieIdSchema }, 'params'),
     validationHandler(updateMovieSchema),
     async function (req, res, next) {
@@ -133,6 +138,7 @@ const moviesApi = (app) => {
   router.delete(
     '/:movieId',
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['delete:movies']),
     validationHandler({ movieId: movieIdSchema }, 'params'),
     async function (req, res, next) {
       const { movieId } = req.params;
